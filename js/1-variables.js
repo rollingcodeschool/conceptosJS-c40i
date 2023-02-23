8da3811ead1270f239fac37b3d67aa13d519c183
Emilse Arias <emilsearias@gmail.com>
2023-02-23 11:39:53 -0300

    declaramos una variable y la mostramos por consola

---
+++ js/1-variables.js
@@ -0,0 +1,14 @@
+//Esto es un comentario de una sola linea
+
+/*
+Esto es un comentario
+de multiples lineas
+*/
+
+// como declarar o crear una variable  (var - let - const)
+
+let pelicula = 'Titanic';
+
+// mostrar una variable por consola
+console.log('Hola mundo');
+console.log(pelicula);
