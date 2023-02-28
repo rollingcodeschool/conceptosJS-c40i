f594e422aa2fb7aa30e7041c9412b7d1df3578bb
Emilse Arias <emilsearias@gmail.com>
2023-02-28 11:22:09 -0300

    agregamos mas condiciones en el ejercicio de votacion

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -11,16 +11,17 @@ if(condicion){
 // pedir la edad a un usuario
 // si tiene desde 16 años tiene que votar
 // menores de 16 no votan
+// opcionales 71 en adelante, 16 y 17
+// desde 18 hasta los 70 inclusive
 
-let edad = parseInt(prompt('Ingrese su edad'));
+let edad = parseInt(prompt("Ingrese su edad"));
 console.log(edad);
-console.log(typeof(edad));
+console.log(typeof edad);
 
-if( edad >= 16 ){
-    document.write('Puede votar');
-}else{
-    document.write('No puede votar');
+if ((edad >= 16 && edad < 18) || edad >= 71) {
+  document.write("es optativo votar");
+} else if (edad >= 18 && edad <= 70) {
+  document.write("Debe votar");
+} else {
+  document.write("No puede votar");
 }
-
-
-
