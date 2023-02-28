c27dd0dea5a0a61dd27df5f03d5415e1fab9047e
Emilse Arias <emilsearias@gmail.com>
2023-02-28 10:23:11 -0300

    agregamos el document write, alert y constantes

--- js/1-variables.js
+++ js/1-variables.js
@@ -8,7 +8,27 @@ de multiples lineas
 // como declarar o crear una variable  (var - let - const)
 
 let pelicula = 'Titanic';
+const URLCampus = 'https://campus.rollingcodeschool.com/';
 
 // mostrar una variable por consola
-console.log('Hola mundo');
+console.log('Hola mundo, voy a ver ' + pelicula + ' 3D');
 console.log(pelicula);
+
+//mostrar un mensaje en el doc. html
+document.write('<h1>Estrenos</h1>');
+document.write(`<p>hoy es martes y voy a ver ${pelicula} 3D</p>`)
+
+//mostrar una ventana
+alert('primer alert');
+
+document.write(`Campus: ${URLCampus}`)
+
+// modificar el valor de una variable
+pelicula = 'Ant man 3';
+document.write(`<p>hoy es martes y voy a ver ${pelicula} 3D</p>`)
+
+
+
+
+
+
