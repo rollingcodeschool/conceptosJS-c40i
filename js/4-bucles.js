56820b86c00634192a609cf72b1ab93ccb3ef030
Emilse Arias <emilsearias@gmail.com>
2023-03-02 11:45:08 -0300

    agregamos el bucle while con un ejemplo breve

---
+++ js/4-bucles.js
@@ -0,0 +1,20 @@
+// while
+/*
+while(codicion logica){
+    todo el codigo que quiero repetir
+    alguna linea que haga que mi condicion logica deje de cumplir
+}
+*/
+
+let contador = 2;
+
+while(contador <= 10){
+    document.write('Este es el parrafo N'+(contador)+'<br>');
+    // contador++; // contador = contador + 1;
+    contador += 2; // contador= contador +2;
+}
+
+
+// do-while
+
+// for
