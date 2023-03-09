f1282a28bb9eb3e863e9c2038ff2ce7d13dbb132
Emilse Arias <emilsearias@gmail.com>
2023-03-09 10:25:59 -0300

    dibujamos una tabla con las filas y columnas que ingreso el usuario

---
+++ js/ejercicios/ejercicio10-tp2.js
@@ -0,0 +1,20 @@
+// 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+
+const filas = parseInt(prompt('Ingrese un numero de filas'));
+const columnas = parseInt(prompt('Ingrese un numero de columnas'));
+
+let numeroCelda = filas * columnas; 
+
+document.write('<table><tbody>');
+//este bucle dibuja filas
+for(let indiceFilas=0; indiceFilas< filas ; indiceFilas++){
+    document.write('<tr>')
+        for(let indiceColumnas=0; indiceColumnas < columnas; indiceColumnas++){
+            document.write(`<td>1</td>`)
+        }
+
+    document.write(' </tr>')
+}
+
+
+document.write('</tbody></table>')
