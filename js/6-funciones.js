c8594821e82170947c1464696c5595f5f367acce
Emilse Arias <emilsearias@gmail.com>
2023-03-14 11:33:28 -0300

    agregamos un ejercicio de funciones

--- js/6-funciones.js
+++ js/6-funciones.js
@@ -42,5 +42,12 @@ nuevoSaludo('connor', 'coolero', 'cruza de potrillo y de perra');
 
 // TODO Pedir a un usuario un nombre y precio de un juego, crear un funcion que informe el precio final en pesos argentinos. Preguntar si quiere saber el precio de otro juego.
 
+const precioFinal = precio => (precio * 1.75).toFixed(2);
 
+do{
 
+const nombreJuego = prompt('Ingrese el nombre del juego');
+const precio = parseFloat(prompt('Ingrese el precio del juego'));
+alert(`El juego ${nombreJuego} cuesta $${precioFinal(precio)} pesos argentinos.`)
+
+}while(confirm('¿Desea consultar otro juego?'));
