92a14c415222dce6f238f951533adce673b5aa0a
Emilse Arias <emilsearias@gmail.com>
2023-03-16 12:02:35 -0300

    agregamos un objeto con notacion literal

---
+++ js/7-objetos.js
@@ -0,0 +1,16 @@
+//objetos notacion literal
+
+// XXX-55555-XXX
+
+let cancion = {
+    // propiedades clave: valor
+    id: 1000, 
+    nombre: 'summer rains',
+    duracion: '2min 40seg',
+    autor: 'Facu Carabajal'
+}
+
+console.log(cancion);
+document.write(cancion.nombre);
+document.write(cancion.autor);
+document.write(cancion['duracion']);
