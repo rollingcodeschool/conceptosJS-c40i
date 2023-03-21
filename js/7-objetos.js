68be64090c8a4839f75c1057ba08afaec4dd7f90
Emilse Arias <emilsearias@gmail.com>
2023-03-21 10:16:17 -0300

    agregamos propiedades y metodos al objeto cancion

--- js/7-objetos.js
+++ js/7-objetos.js
@@ -3,14 +3,47 @@
 // XXX-55555-XXX
 
 let cancion = {
-    // propiedades clave: valor
-    id: 1000, 
-    nombre: 'summer rains',
-    duracion: '2min 40seg',
-    autor: 'Facu Carabajal'
-}
+  // propiedades clave: valor
+  id: 1000,
+  nombre: "summer rains",
+  duracion: "2min 40seg",
+  autor: "Facu Carabajal",
+  //metodos
+  play: function () {
+    console.log(this)
+    document.write(`<p>La cancion ${this.nombre} comenzo ...</p>`);
+  },
+  pausa: () => {
+    console.log(this)
+    document.write(`<p>Se pauso la canción</p>`);
+  }
+};
 
+//mostrar el objeto
 console.log(cancion);
-document.write(cancion.nombre);
-document.write(cancion.autor);
-document.write(cancion['duracion']);
+// document.write(cancion);
+document.write(`Cancion: ${cancion.nombre}<br>`);
+document.write(`Autor: ${cancion.autor}<br>`);
+document.write(`Duracion: ${cancion["duracion"]}`);
+
+//metodo del objeto Object devuelve true o false
+console.log(cancion.hasOwnProperty("autor"));
+console.log(cancion.hasOwnProperty("estado"));
+
+cancion.play();
+cancion.pausa();
+
+console.log(this);
+
+//agregar propiedades
+cancion.estado = true;
+console.log(cancion);
+document.write(`Estado: ${cancion.estado}<br>`);
+
+//modificar la propiedad
+cancion.estado = false;
+document.write(`Estado: ${cancion.estado}<br>`);
+
+//borrar una propiedad del objeto
+delete cancion.estado;
+console.log(cancion);
